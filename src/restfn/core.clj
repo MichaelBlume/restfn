31166de5818832c1face4455ea941034aa4cf381
Mike Blume <mike@loggly.com>
2013-04-09 11:12:04 -0700

    source

---
+++ src/restfn/core.clj
@@ -0,0 +1,59 @@
+(ns restfn.core
+  (:import (java.io StringWriter PrintWriter)
+           (clojure.lang IFn ArityException))
+  (:use (clojure [string :only [split]])
+        (cheshire [core :only [generate-string]])))
+
+(defn- try-parse-int [s]
+  (try (Integer/parseInt s)
+    (catch NumberFormatException e s)))
+
+(defprotocol RestSerializable
+  (rest-serialize [this]
+   "Convert to something Cheshire can JSONify"))
+
+(extend Object
+  RestSerializable
+  {:rest-serialize identity})
+
+(defn- descend-one [obj-raw seg-raw]
+  (let [obj (rest-serialize obj-raw)
+        seg (try-parse-int seg-raw)]
+    (if (instance? IFn obj)
+      (obj seg)
+      (.get obj seg))))
+
+(defn- do-result [raw-result req]
+  (let [result (if (seq? raw-result)
+                 (map rest-serialize raw-result)
+                 (rest-serialize raw-result))]
+    (if (map? result)
+      (if-let [action-fn (result (:request-method req))]
+        (try (action-fn req)
+          (catch ArityException _ (action-fn)))
+        result)
+      result)))
+
+(defn- exc->str [^Throwable e]
+  (let [wrtr (StringWriter.)
+        pwrtr (PrintWriter. wrtr)]
+    (.printStackTrace e pwrtr)
+    (str wrtr)))
+
+(defn get-rest-handler [rest-tree]
+  (fn [req]
+    (try
+      (let [uri      (:uri req)
+            uri-segs (remove empty? (split uri #"/"))
+            result   (reduce descend-one rest-tree uri-segs)
+            body-obj (do-result result req)
+            body     (str
+                       (generate-string body-obj {:pretty true})
+                       \newline)]
+        {:status 200
+         :headers {"Content-Type" "application/json"}
+         :body body})
+      (catch Throwable e
+        {:status 500
+         :headers {"Content-Type" "text/plain"}
+         :body (exc->str e)}))))
---
+++ test/restfn/test.clj
@@ -0,0 +1,71 @@
+(ns restfn.test
+  (:import (java.util ArrayList))
+  (:use (clojure test)
+        (clojure [string :only [split-lines]])
+        (restfn core)
+        (cheshire [core :only [parse-string]])))
+
+(defn java-list [& args]
+  (let [ret (ArrayList.)]
+    (doseq [x args]
+      (.add ret x))
+    ret))
+
+(extend java.util.regex.Pattern
+  RestSerializable
+  {:rest-serialize (fn [r] #(re-matches r %))})
+
+(def handler
+  (get-rest-handler
+    {"list" [1 3 5]
+     "map" {"foo" "bar"}
+     "javalist" (java-list 5 9 7)
+     "simplefn" inc
+     "complexfn" (fn [x] {"key" x})
+     "pattern" #"(.*)and(.*)"
+     "seq" (for [x (range 3)]
+             (reify RestSerializable (rest-serialize [_] x)))
+     "rest" {:post (fn [req] (:req-key req))
+             :delete (fn [] "deleted")}}))
+
+(defn object-from-request [req]
+  (let [res (handler req)]
+    (is (= (:status res) 200))
+    (is (= (:headers res) {"Content-Type" "application/json"}))
+    (parse-string (:body (handler req)))))
+
+(defn object-from-uri [uri]
+  (object-from-request {:uri uri :request-method :get}))
+
+(deftest simplegets
+  (are [uri result]
+    (= result (object-from-uri uri))
+    "list" [1 3 5]
+    "/list/" [1 3 5]
+    "/list/0" 1
+    "/list/1" 3
+    "map" {"foo" "bar"}
+    "map/foo" "bar"
+    "javalist/" [5 9 7]
+    "javalist/0" 5
+    "simplefn/5" 6
+    "complexfn/3" {"key" 3}
+    "complexfn/hello/key" "hello"
+    "pattern/fooandbar" ["fooandbar" "foo" "bar"]
+    "seq" [0 1 2]))
+
+(deftest testrest
+  (are [req result]
+    (= result (object-from-request req))
+    {:uri            "rest"
+     :request-method :post
+     :req-key        [1 2 3]} [1 2 3]
+    {:uri            "rest"
+     :request-method :delete} "deleted"))
+
+(deftest testerror
+  (let [result (handler {:uri "list/5" :request-method :get})]
+    (is (= (:status result) 500))
+    (is (= (:headers result) {"Content-Type" "text/plain"}))
+    (is (re-matches #".*IndexOutOfBoundsException.*"
+                    ((split-lines (:body result)) 0)))))

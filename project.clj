a98338f4de68585cdc1f4aa6558aeaa4a9dfa6f1
Mike Blume <mike@loggly.com>
2013-07-07 16:54:48 -0700

    start 0.0.4 development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.michaelblume/restfn "0.0.3"
+(defproject org.clojars.michaelblume/restfn "0.0.4-SNAPSHOT"
   :description "A simple lib for creating admin REST APIs"
   :url "https://github.com/MichaelBlume/restfn"
   :license {:name "Eclipse Public License"

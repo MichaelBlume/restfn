4a92a53a517eeb57adce2300ef09563c23687fee
Mike Blume <mike@loggly.com>
2014-03-25 14:37:28 -0700

    upgrade clojure dependency

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :url "https://github.com/MichaelBlume/restfn"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [cheshire            "5.3.1"]])
